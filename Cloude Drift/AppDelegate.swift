f4314d402cf57295885d0a7adcf8331e0f5bba5e
abhishek kushwaha <kushabhee@icloud.com>
2024-12-31 20:58:28 +0545

    Initial Commit

---
+++ Cloude Drift/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Cloude Drift
+//
+//  Created by Abhishek Kushwaha on 31/12/2024.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
---
+++ Cloude DriftTests/Cloude_DriftTests.swift
@@ -0,0 +1,17 @@
+//
+//  Cloude_DriftTests.swift
+//  Cloude DriftTests
+//
+//  Created by Abhishek Kushwaha on 31/12/2024.
+//
+
+import Testing
+@testable import Cloude_Drift
+
+struct Cloude_DriftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
